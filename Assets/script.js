0ca94b72d48cbd8a63da58e0ca40566232be1c9e
CHENG CHIEN <hcc780921@gmail.com>
2022-01-22 00:10:18 -0800

    js modified

--- Assets/script.js
+++ Assets/script.js
@@ -5,4 +5,7 @@ $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm a'));
 }
 
 var dt = new Date();
-document.getElementById("#currentDay").innerHTML = (("0"+dt.getDate()).slice(-2)) +"."+ (("0"+(dt.getMonth()+1)).slice(-2)) +"."+ (dt.getFullYear()) +" "+ (("0"+dt.getHours()).slice(-2)) +":"+ (("0"+dt.getMinutes()).slice(-2));
+document.getElementById("currentDay").innerHTML = (("0"+dt.getDate()).slice(-2)) +"."+ (("0"+(dt.getMonth()+1)).slice(-2)) +"."+ (dt.getFullYear()) +" "+ (("0"+dt.getHours()).slice(-2)) +":"+ (("0"+dt.getMinutes()).slice(-2));
+document.getElementById("current").innerHTML = dt.toLocaleTimeString()
+
+function
