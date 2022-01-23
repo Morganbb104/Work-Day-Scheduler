28878a06c23ed719c49c421e5823dcd4c63b6c75
CHENG CHIEN <hcc780921@gmail.com>
2022-01-22 23:31:31 -0800

    add time blcok color in jss

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,31 @@
-// function (event){
-//   let getItemTime;
-// // display the date and time by [Moment.js]
-// $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm a'));  
-// }
-
-
-var arrayHoursId = ["#hour6", "#hour7", "#hour8", "#hour9", "#hour10", "#hour11", "#hour12", "#hour13", "#hour14", "#hour15", "#hour16", "#hour17", "#hour18", "#hour19", "#hour20","#hour21"]
 
 // present the current time
 var today = moment();
 $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm a'));
+
+//change time block color as past,present and future
+function timeBlockColor() {
+  //current number of hours by [Moment.js]
+  var currentTime = moment().hour();
+
+  $(".time-block").each(function() {
+      // turn #hours to mumber          
+      var hourInBlock = parseInt($(this).attr("id").split("hour")[1]);
+      console.log(currentTime, hourInBlock);
+          // $(this).removeClass("present");
+          // $(this).removeClass("future");
+          // $(this).removeClass("future");
+      if (hourInBlock < currentTime) {
+          $(this).addClass("past");
+
+      } else if (hourInBlock = currentTime) {
+          $(this).addClass("present");
+      } else if(hourInBlock > currentTime) {
+          $(this).addClass("future");
+      }
+  })
+
+}
+timeBlockColor()
+
+
