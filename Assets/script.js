6329063842d97714dbd3346d95b4babf162372bf
CHENG CHIEN <hcc780921@gmail.com>
2022-01-23 14:44:03 -0800

    click save Btn everthing will be seen next time

--- Assets/script.js
+++ Assets/script.js
@@ -24,4 +24,42 @@ function timeBlockColor() {
 }
 timeBlockColor()
 
+// A fuction to save the content from textarea into localstorage
+
+// make an array, 
+var Inputtext = JSON.parse(localStorage.getItem('.description')) || [];
+
+// click Btn to save
+$(".saveBtn").on('click',function(){
+  console.log($(this).siblings(".description").val());
+  console.log($(this).parent().attr('id'))
+  var obj = {
+    id:$(this).parent().attr('id'),
+    text:$(this).siblings(".description").val(),
+  }
+  Inputtext.push(obj);
+  localStorage.setItem('.description', JSON.stringify(Inputtext));
+  console.log(obj);
+})
+
+function showtext(){
+  $(".time-block").each(function(){
+    console.log($(this).attr('id'))
+    console.log($(this).children('.description'))
+    for(i=0;i < Inputtext.length; i++){
+      if (Inputtext[i].id === $(this).attr('id') ) {
+        $(this).children('.description').val(Inputtext[i].text)
+      }
+    }
+  })
+
+}
+
+showtext()
+
+$(".clear").on('click',function(){
+  localStorage.clear(); // clean localstorage
+  window.location.reload();  // refresh and reload
+})
+
 
