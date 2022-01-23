f7d3e7c5b559b2ef8872799e1d25de88ab3f9447
CHENG CHIEN <hcc780921@gmail.com>
2022-01-23 14:55:43 -0800

    js comments

--- Assets/script.js
+++ Assets/script.js
@@ -31,21 +31,21 @@ var Inputtext = JSON.parse(localStorage.getItem('.description')) || [];
 
 // click Btn to save
 $(".saveBtn").on('click',function(){
-  console.log($(this).siblings(".description").val());
-  console.log($(this).parent().attr('id'))
+  // console.log($(this).siblings(".description").val());
+  // console.log($(this).parent().attr('id'))
   var obj = {
     id:$(this).parent().attr('id'),
     text:$(this).siblings(".description").val(),
   }
   Inputtext.push(obj);
   localStorage.setItem('.description', JSON.stringify(Inputtext));
-  console.log(obj);
+  // console.log(obj);
 })
 
 function showtext(){
   $(".time-block").each(function(){
-    console.log($(this).attr('id'))
-    console.log($(this).children('.description'))
+    // console.log($(this).attr('id'))
+    // console.log($(this).children('.description'))
     for(i=0;i < Inputtext.length; i++){
       if (Inputtext[i].id === $(this).attr('id') ) {
         $(this).children('.description').val(Inputtext[i].text)
@@ -54,7 +54,6 @@ function showtext(){
   })
 
 }
-
 showtext()
 
 $(".clear").on('click',function(){
